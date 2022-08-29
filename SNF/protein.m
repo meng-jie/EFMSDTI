da0b5481ff4f8e3d56d70213ec8b232338a881aa
meng-jie <1217435660@qq.com>
2022-08-29 18:08:21 +0800

    Add files via upload

--- SNF/protein.m
+++ SNF/protein.m
@@ -1,1 +1,37 @@
-.682243 12.295082 11.343284 12.149533 9.500000 12.149533 9.813084 13.084112 11.448598 12.017167 12.751678 10.981308 12.578616 10.000000 16.588785 11.682243 17.289720 12.292359 19.230769 12.383178 10.759494 11.377246 10.280374 10.835913 12.135922 12.849162 9.785933 11.073826 11.448598 10.981308 9.638554 9.813084 11.929825 9.793814 11.784512 10.514019 10.626703 13.084112 10.981308 8.878505 11.744966 12.166172 17.543860 11.587983 12.925170 10.747664 19.210526 12.149533 13.966480 10.747664 12.149533 9.813084 10.483871 10.747664 10.981308 10.280374 17.990654 11.682243 11.682243 11.904762 10.514019 11.397059 19.893899 11.682243 11.448598 12.328767 11.214953 13.824885 11.448598 10.514019 9.708738 11.682243 11.200000 13.502110 11.915888 9.139785 10.046729 10.514019 9.345794 11.707317 14.666667 13.548387 18.159806 13.888889 11.448598 11.915888 12.149533 12.149533 14.592275 24.242424 10.000000 10.738255 11.229947 10.046729 10.498688 17.289720 10.046729 10.320285 11.448598 11.111111 9.693878 10.280374 20.000000 10.046729 12.500000 10.747664 10.046729 10.981308 11.111111 13.725490 10.280374 11.214953 12.616822 11.682243 10.280374 10.514019 13.291139 9.842520 10.280374 10.344828 12.800000 12.616822 10.046729 17.632850 11.915888 11.016949 11.953353 12.149533 11.448598 11.915888 20.000000 11.052632 10.747664 20.754717 11.397059 10.046729 10.764873 10.981308 10.070258 10.650888 12.383178 11.356467 9.708738 10.843373 12.188366 10.514019 11.214953 12.149533 11.448598 19.487179 12.707182 12.000000 10.810811 12.307692 12.857143 12.033195 11.640212 12.422360 11.682243 10.747664 11.915888 11.91588
+clear
+close all
+%%%Load the data
+proteinprotein = load('../data/proteinprotein.txt');
+sim_proteinDisease = load('../simNet/Sim_proteinDisease.txt');
+proteinsim1network = load('../data/proteinsim1network.txt');
+
+%%%First, set all the parameters.
+K = 20;%number of neighbors, usually (10~30)
+alpha = 0.5; %hyperparameter, usually (0.3~0.8)
+T = 20; %Number of Iterations, usually (10~20)
+
+%If the data are all continuous values, we recommend the users to perform standard normalization before using SNF, though it is optional depending on the data the users want to use.
+Proteinprotein = Standard_Normalization(proteinprotein);
+Sim_proteinDisease = Standard_Normalization(sim_proteinDisease);
+Proteinsim1network = Standard_Normalization(proteinsim1network);
+
+
+%%%Calculate the pair-wise distance; If the data is continuous, we recommend to use the function "dist2" as follows;
+%if the data is discrete, we recommend the users to use chi-square distance
+
+Dist1 = dist2(Proteinprotein,Proteinprotein);
+Dist2 = dist2(Sim_proteinDisease,Sim_proteinDisease);
+Dist3 = dist2(Proteinsim1network,Proteinsim1network);
+
+
+%%%next, construct similarity graphs
+
+W1 = affinityMatrix(Dist1, K, alpha);
+W2 = affinityMatrix(Dist2, K, alpha);
+W3 = affinityMatrix(Dist3, K, alpha);
+
+
+% then the overall matrix can be computed by similarity network fusion(SNF):
+Wp = SNF({W1,W2,W3},K,T);
+
+save('../fusion/three_type.mat','Wp');
