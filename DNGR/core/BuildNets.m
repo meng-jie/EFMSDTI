916ed1411a56532ea2a9349349ecd38b85b80909
meng-jie <1217435660@qq.com>
2022-08-29 18:31:58 +0800

    Add files via upload

--- DNGR/core/BuildNets.m
+++ DNGR/core/BuildNets.m
@@ -1,49 +1,26 @@
-6	0.358
-DB09049	DB00521	0.597
-DB09049	DB00953	0.437
-DB09049	DB04398	0.145
-DB09049	DB01361	0.549
-DB09049	DB00257	0.275
-DB09049	DB00982	0.221
-DB09049	DB00636	0.271
-DB09049	DB00191	0.206
-DB09049	DB01076	0.452
-DB09049	DB01239	0.361
-DB09049	DB06777	0.462
-DB09049	DB01294	0.333
-DB09049	DB00373	0.563
-DB09049	DB00712	0.147
-DB09049	DB01624	0.493
-DB09049	DB01183	0.869
-DB09049	DB00877	0.681
-DB09049	DB00405	0.353
-DB09049	DB01245	0.303
-DB09049	DB06770	0.15
-DB09049	DB00928	0.475
-DB09049	DB00374	0.578
-DB09049	DB00715	0.577
-DB09049	DB01184	0.481
-DB09049	DB01623	0.409
-DB09049	DB08909	0.349
-DB09049	DB01242	0.408
-DB09049	DB00402	0.506
-DB09049	DB00870	0.197
-DB09049	DB00147	0.406
-DB09049	DB08799	0.4
-DB09049	DB01366	0.384
-DB09049	DB00526	0.438
-DB09049	DB00281	0.319
-DB09049	DB00985	0.535
-DB09049	DB00250	0.15
-DB09049	DB01410	0.535
-DB09049	DB01071	0.418
-DB09049	DB00196	0.387
-DB09049	DB00631	0.429
-DB09049	DB00308	0.412
-DB09049	DB01129	0.5
-DB09049	DB00769	0.643
-DB09049	DB01599	0.25
-DB09049	DB00692	0.507
-DB09049	DB00135	0.328
-DB09049	DB00554	0.344
-DB09049
+%% this is the configuration file of stacked denoising autoencoder ≈‰÷√Œƒº˛%%
+
+num_nodes = dim;                    %number of vertex
+nnsize = [num_nodes 500 200 100];        %layer-wised setting
+len = length(nnsize);
+
+rand('state',0)
+sae = saesetup(nnsize);
+
+for i = 1: len - 1
+    sae.ae{i}.activation_function       = 'sigm';
+    sae.ae{i}.dropoutFraction           = 0;          %  Dropout fraction, only used for fine tuning
+    sae.ae{i}.momentum                  = 0;          %  Momentum
+    sae.ae{1}.scaling_learningRate      = 0.95;          %  Scaling factor for the learning rate (each epoch)
+    
+    sae.ae{i}.nonSparsityPenalty        = 0;          %  0 indicates Non sparsity penalty
+    sae.ae{i}.sparsityTarget            = 0.01;       %  Sparsity target
+    sae.ae{i}.inputZeroMaskedFraction   = 0.2;        %  Used for Denoising AutoEncoders
+end
+
+sae.ae{1}.learningRate              = 0.25;
+sae.ae{2}.learningRate              = 0.25;
+sae.ae{3}.learningRate              = 0.25;
+
+opts.numepochs = 100;
+opts.batchsize = 50;
